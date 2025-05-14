90d8280690f96fe57b2258830253c4b7fba0b1cf
Mike <mclancy96@gmail.com>
2025-05-14 14:32:07 -0400

    Element added on submission

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-const submitButton = document.querySelector('input[type="submit"]')
+const submitButton = document.querySelector('input[type="submit"]');
+const taskList = document.getElementById('tasks');
+const taskDescription = document.getElementById('new-task-description').value
+
+const addElement = (task) => {
+  const newEl = document.createElement('li');
+  newEl.textContent = task;
+  taskList.appendChild(newEl)
+}
 
 const submitClick = (buttonClick) => {
   buttonClick.preventDefault()
+  addElement(taskDescription)
 }
 
 document.addEventListener("DOMContentLoaded", () => {
