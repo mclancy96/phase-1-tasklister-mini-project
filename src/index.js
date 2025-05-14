cbab90724945dc17c01587af98e4c1e404120905
Mike <mclancy96@gmail.com>
2025-05-14 14:40:06 -0400

    Prevent empty tasks

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 const submitButton = document.querySelector('input[type="submit"]');
 const taskList = document.getElementById('tasks');
-const taskDescription = document.getElementById('new-task-description').value
+const taskDescription = document.getElementById('new-task-description')
 
-const addElement = (task) => {
+const addElementToList = (task) => {
   const newEl = document.createElement('li');
   newEl.textContent = task;
-  taskList.appendChild(newEl)
+  taskList.appendChild(newEl);
+  taskDescription.value = '';
 }
 
-const submitClick = (buttonClick) => {
+const submitClickAction = (buttonClick) => {
   buttonClick.preventDefault()
-  addElement(taskDescription)
+  if (taskDescription.value != '') {
+    addElementToList(taskDescription.value)
+  } else {
+    taskDescription.placeholder = 'Text Cannot be Blank!'
+  }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  submitButton.addEventListener('click', submitClick)
+  submitButton.addEventListener('click', submitClickAction)
 });
